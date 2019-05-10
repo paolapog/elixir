64ee532b1dc1fd3a55e72cecc0454f88f079208d
paolapog <paola.poggini@prima.it>
2019-05-10 17:02:38 +0200

    nucleotide

---
+++ nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,34 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    Enum.count(strand, &(&1 == nucleotide))
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    @nucleotides
+    |> Enum.map(fn nucleotide -> %{nucleotide => count(strand, nucleotide)} end)
+    |> Enum.reduce(%{}, fn nucleotide, acc -> Map.merge(nucleotide, acc) end)
+  end
+end
