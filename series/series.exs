ef99c138fd1e4675b32cbd5516a11fb02535fde9
paolapog <paola.poggini@prima.it>
2019-05-15 12:01:44 +0200

    series

---
+++ series/series.exs
@@ -0,0 +1,16 @@
+defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(_s, size) when size <= 0, do: []
+
+  def slices(s, size) do
+    s
+    |> String.graphemes
+    |> Enum.chunk_every(size, 1, :discard)
+    |> Enum.map(&Enum.join/1)
+  end
+end
