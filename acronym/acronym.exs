2c41fcb80d158779539d2e9ce8b6ba034dc527bc
paolapog <paola.poggini@prima.it>
2019-05-16 09:54:55 +0200

    fallisce un test

---
+++ acronym/acronym.exs
@@ -0,0 +1,19 @@
+defmodule Acronym do
+  @doc """
+  Generate an acronym from a string.
+  "This is a string" => "TIAS"
+  """
+  @spec abbreviate(String.t()) :: String.t()
+  def abbreviate(string) do
+    string
+    |> string_split()
+    |> Enum.reduce("", fn single_string, acc -> String.first(single_string)  <> acc end)
+    |> String.upcase()
+    |> String.reverse()
+  end
+
+  def string_split(string) do
+    string
+    |> String.split(~r/[\s,-]/, trim: true)
+  end
+end
