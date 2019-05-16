ddc977bdb480b61b8a48b347c05ec7e09e85b9f4
paolapog <paola.poggini@prima.it>
2019-05-16 15:18:42 +0200

    risolto test fallito, completo

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -7,13 +7,15 @@ defmodule Acronym do
   def abbreviate(string) do
     string
     |> string_split()
-    |> Enum.reduce("", fn single_string, acc -> String.first(single_string)  <> acc end)
+    |> Enum.reduce("", fn single_string, acc -> String.first(single_string) <> acc end)
     |> String.upcase()
     |> String.reverse()
   end
 
   def string_split(string) do
     string
-    |> String.split(~r/[\s,-]/, trim: true)
+    |> Macro.underscore()
+    |> String.split(~r/[\s,-_]/, trim: true)
+
   end
 end
