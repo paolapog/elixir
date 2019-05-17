089f4226f340e2b79b82ece1992d21e8f6edbe66
paolapog <paola.poggini@prima.it>
2019-05-17 14:41:13 +0200

    beer

---
+++ beer-song/beer_song.exs
@@ -0,0 +1,39 @@
+defmodule BeerSong do
+  @doc """
+  Get a single verse of the beer song
+  """
+  @spec verse(integer) :: String.t()
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+  def verse(number) do
+
+    bot = if number - 1 == 1, do: "bottle", else: "bottles"
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} #{bot} of beer on the wall.
+    """
+  end
+
+
+  @doc """
+  Get the entire beer song for a given range of numbers of bottles.
+  """
+  @spec lyrics(Range.t()) :: String.t()
+  def lyrics(), do: lyrics(99..0)
+
+  def lyrics(range) do
+    Enum.map(range, fn string -> verse(string) end)
+    |> Enum.join("\n")
+  end
+end
